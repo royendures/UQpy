222d07f9813c8646fa4750314ae181a0badbd23c
Sumanta Roy <sroy41@jhu.edu>
2026-08-28 11:03:25 -0400

    feat: Add doctrings

--- src/UQpy/distributions/collection/Triangular.py
+++ src/UQpy/distributions/collection/Triangular.py
@@ -3,5 +3,10 @@ import scipy.stats as stats
 
 class Triangular(DistributionContinuous1D):
     def __init__(self, c, loc=0., scale=1.):
+        """
+        param c: shape
+        param loc: loc
+        param scale: scale
+        """
         super().__init__(c=c, loc=loc, scale=scale, ordered_parameters=("c", "loc", "scale"))
         self._construct_from_scipy(scipy_name=stats.triang)
