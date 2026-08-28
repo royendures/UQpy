8a4961705e09b55ff82d5e5121b7765c73ae033d
Sumanta Roy <sroy41@jhu.edu>
2026-08-28 10:40:26 -0400

    feat: Add missing values to triangular 1D distribution

--- src/UQpy/distributions/collection/Triangular.py
+++ src/UQpy/distributions/collection/Triangular.py
@@ -2,6 +2,6 @@ from UQpy.distributions.baseclass import DistributionContinuous1D
 import scipy.stats as stats
 
 class Triangular(DistributionContinuous1D):
-    def __init__(self, c, loc, scale):
-        super().__init__(c=c, loc=loc, scale=scale, ordered_parameters=("loc", "scale"))
+    def __init__(self, c, loc=0., scale=1.):
+        super().__init__(c=c, loc=loc, scale=scale, ordered_parameters=("c", "loc", "scale"))
         self._construct_from_scipy(scipy_name=stats.triang)
